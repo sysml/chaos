ebcb25d73e99a810246bd602bc92b11608cecb2b
Florian Schmidt <florian.schmidt@neclab.eu>
2017-01-23 14:55:30 +0100

    Daemon now precreates shells and destroys them on shutdown

--- bin/shell_daemon.c
+++ bin/shell_daemon.c
@@ -42,28 +42,36 @@
 #include <sys/wait.h>
 #include <stdio.h>
 
+#include <h2/h2.h>
+#include <h2/xen.h>
+#include <h2/xen/dev.h>
 #include <shell_daemon/cmdline.h>
 
 #define ERROR(format...) syslog(LOG_DAEMON | LOG_ERR, format)
 #define NOTICE(format...) syslog(LOG_DAEMON | LOG_NOTICE, format)
 #define INFO(format...) syslog(LOG_DAEMON | LOG_INFO, format)
 
+/* TODO: UDS name should be configurable */
+static char *sockname = "/tmp/shell_daemon_socket";
+#define MAX_SHELLS 10000UL
+
 /* Global state */
 struct {
     bool socket_created;
     int sockfd;
     bool shutting_down;
+    bool shell_initialized;
+    h2_ctx *ctx;
+    struct h2_guest *shell[MAX_SHELLS];
+    unsigned long remaining_shells;
+    uint16_t last_ipaddr;
 } global;
-
-/* TODO: UDS name should be configurable */
-static char *sockname = "/tmp/shell_daemon_socket";
 
 void shutdown_shell_daemon(void)
 {
     /* close socket so accept() stops blocking */
     if (global.socket_created) {
         close(global.sockfd);
-        unlink(sockname);
     }
     global.shutting_down = true;
 }
@@ -238,6 +246,145 @@ void wait_for_sockdata(void) {
     }
 }
 
+h2_guest* precreate_shell(unsigned long ind, h2_hyp_cfg* cfg, unsigned long memory)
+{
+    int ret;
+    int i;
+    h2_guest* shell;
+
+    ret = h2_guest_alloc(&shell, h2_hyp_t_xen);
+    if (ret) {
+        ERROR("Allocating shell failed with error code %d.\n", ret);
+        return NULL;
+    }
+
+    shell->name = strdup("h2g");
+    shell->cmdline = strdup("");
+    shell->memory = memory;
+    shell->vcpus.count = 1;
+    shell->address_size = 64;
+    shell->paused = false;
+
+    shell->kernel.type = h2_kernel_buff_t_file;
+    /* This is set on actual creation
+    shell->kernel.buff.file.k_path
+    shell->kernel.buff.file.rd_path */
+
+    shell->hyp.guest.xen->pvh = false;
+    shell->hyp.guest.xen->xs.active = true;
+#ifdef CONFIG_H2_XEN_NOXS
+    shell->hyp.guest.xen->noxs.active = true;
+#endif
+    shell->hyp.guest.xen->console.active = true;
+    shell->hyp.guest.xen->console.meth = h2_xen_dev_meth_t_xs;
+    shell->hyp.guest.xen->console.be_id = 0;
+    shell->hyp.guest.xen->devs[1].type = h2_xen_dev_t_vif;
+    shell->hyp.guest.xen->devs[1].dev.vif.id = 0;
+    shell->hyp.guest.xen->devs[1].dev.vif.backend_id = 0;
+    shell->hyp.guest.xen->devs[1].dev.vif.meth = h2_xen_dev_meth_t_xs;
+    // increment IP address...
+    global.last_ipaddr++;
+    // .. but make sure to skip a.b.c.0 and a.b.c.255
+    if ((global.last_ipaddr&0xff) == 0) {
+        global.last_ipaddr += 1;
+    }
+    else if ((global.last_ipaddr&0xff) == 0xff) {
+        global.last_ipaddr += 2;
+    }
+    shell->hyp.guest.xen->devs[1].dev.vif.ip.s_addr = (0x0a80<<16) | (global.last_ipaddr&0xff); /* 10.128.x.y*/
+    shell->hyp.guest.xen->devs[1].dev.vif.mac[0] = 0xde;
+    shell->hyp.guest.xen->devs[1].dev.vif.mac[1] = 0xad;
+    shell->hyp.guest.xen->devs[1].dev.vif.mac[2] = 0xbe;
+    shell->hyp.guest.xen->devs[1].dev.vif.mac[3] = 0xef;
+    // make MAC match IP, easy to remember
+    shell->hyp.guest.xen->devs[1].dev.vif.mac[4] = ((global.last_ipaddr>>8) & 0xff);
+    shell->hyp.guest.xen->devs[1].dev.vif.mac[5] = (global.last_ipaddr & 0xff);
+    shell->hyp.guest.xen->devs[1].dev.vif.bridge = strdup("xenbr");
+
+    h2_open(&global.ctx, h2_hyp_t_xen, cfg);
+
+    ret = h2_xen_domain_precreate(global.ctx->hyp.ctx.xen, shell);
+    if (ret) {
+        ERROR("Precreating shell failed with error code %d.\n", ret);
+        goto out_free;
+    }
+
+    for (i = 0; i < H2_XEN_DEV_COUNT_MAX && !ret; i++) {
+        ret = h2_xen_dev_create(global.ctx->hyp.ctx.xen, shell,
+                                    &(shell->hyp.guest.xen->devs[i]));
+    }
+    if (ret) {
+        ERROR("Failed to initialize Xen device %u with error code %d.\n", i, ret);
+        for (i = 0; i < H2_XEN_DEV_COUNT_MAX; i++) {
+            h2_xen_dev_destroy(global.ctx->hyp.ctx.xen, shell,
+                                    &(shell->hyp.guest.xen->devs[i]));
+        }
+        goto out_free;
+    }
+
+    return shell;
+
+out_free:
+    h2_xen_domain_destroy(global.ctx->hyp.ctx.xen, shell);
+    return NULL;
+}
+
+int precreate_shells(unsigned long shells, unsigned long memory)
+{
+    int i;
+    h2_hyp_cfg cfg;
+
+    if (shells > MAX_SHELLS) {
+        NOTICE("Requested number of precreated shells is above maximum (%lu > %lu), only creating %lu shells.\n",
+                shells, MAX_SHELLS, MAX_SHELLS);
+        shells = MAX_SHELLS;
+    }
+
+    cfg.xen.xs.domid = 0;
+    cfg.xen.xs.active = true;
+#ifdef CONFIG_H2_XEN_NOXS
+    cfg.xen.noxs.active = true;
+#endif
+    cfg.xen.xlib = h2_xen_xlib_t_xc;
+
+    global.remaining_shells = 0;
+    global.last_ipaddr = 0;
+    for (i = 0; i < shells; i++) {
+        global.shell[i] = precreate_shell(i, &cfg, memory);
+        if (!(global.shell[i])) {
+            ERROR("Precreating shell no %u failed, stopping precreation.\n", i);
+            return -ENOMEM;
+        }
+        else {
+            global.remaining_shells++;
+        }
+    }
+    INFO("Precreated %u shells.\n", i);
+    return 0;
+}
+
+int shell_daemon_cleanup(void) {
+    int i;
+    int ret = 0;
+
+    unlink(sockname);
+
+    INFO("Destroying unused precreated shells...\n");
+    for (i = 0; i < global.remaining_shells; i++) {
+        // save first potential error
+        if (!ret) {
+            ret = h2_guest_destroy(global.ctx, global.shell[i]);
+        }
+        else {
+            ret = h2_guest_destroy(global.ctx, global.shell[i]);
+        }
+        h2_guest_free(&global.shell[i]);
+    }
+    h2_close(&global.ctx);
+
+    return ret;
+}
+
 int main(int argc, char **argv)
 {
     int ret;
@@ -263,9 +410,12 @@ int main(int argc, char **argv)
         goto out;
     }
 
+    precreate_shells(cmd.shells, cmd.memory);
+
     while (1) {
         if (global.shutting_down) {
-            ret = 0;
+            ret = shell_daemon_cleanup();
+            INFO("Shutdown done.\n");
             break;
         }
 
